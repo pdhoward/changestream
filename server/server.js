012a27698d0cccbbd412710cb98059aa70d03225
pdhoward <patrick.howard@hotmail.com>
2018-09-10 16:44:03 -0400

    update

--- server/server.js
+++ server/server.js
@@ -30,6 +30,8 @@ app.use('/api', api);
 
 mongoose.connect('mongodb://localhost/tasksdb?replicaSet=rs0');
 
+// mongodb://localhost:27017,localhost:27018,localhost:27019?replicaSet=mongo-repl
+
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Connection Error:'));
@@ -64,4 +66,68 @@ db.once('open', () => {
     }
   });
 });
+
+db.on('open', ()  => {
+      console.log("Connected correctly to server");
+      // specify db and collections
+      //const db = client.db("superheroesdb");
+      const collection = db.collection("superheroes");
+
+      const changeStream = collection.watch(pipeline);
+      // start listen to changes
+      changeStream.on("change", function (change) {
+        console.log("Super Hero Change Log")
+        console.log(change);
+      });
+
+      // insert few data with timeout so that we can watch it happening
+      setTimeout(function () {
+        collection.insertOne({ "batman": "bruce wayne" }, function (err) {
+          assert.ifError(err);
+        });
+      }, 1000);
+
+      setTimeout(function () {
+        collection.insertOne({ "superman": "clark kent" }, function (err) {
+          assert.ifError(err);
+        });
+      }, 2000);
+
+      setTimeout(function () {
+        collection.insertOne({ "wonder-woman": "diana prince" }, function (err) {
+          assert.ifError(err);
+        });
+      }, 3000);
+
+      setTimeout(function () {
+        collection.insertOne({ "ironman": "tony stark" }, function (err) {
+          assert.ifError(err);
+        });
+      }, 4000);
+
+      setTimeout(function () {
+        collection.insertOne({ "spiderman": "peter parker" }, function (err) {
+          assert.ifError(err);
+        });
+      }, 5000);
+
+      // update existing document
+      setTimeout(function () {
+        collection.updateOne({ "ironman": "tony stark" }, { $set: { "ironman": "elon musk" } }, function (err) {
+          assert.ifError(err);
+        });
+      }, 6000);
+
+      // delete existing document
+      setTimeout(function () {
+        collection.deleteOne({ "spiderman": "peter parker" }, function (err) {
+          assert.ifError(err);
+        });
+      }, 7000);
+
+})
+  .catch(err => {
+    console.error(err);
+  });
+
 
