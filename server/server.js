6bcdd12c3e1417b6288a2043ea0bbee0450f5b74
pdhoward <patrick.howard@hotmail.com>
2018-09-10 13:56:28 -0400

    updates

---
+++ server/server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const api = require('./routes/api');
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: '596248',
+  key: '387fa099d2fd683065e2',
+  secret: '7718014b808e397465e7',
+  cluster: 'us2',
+  encrypted: true,
+});
+const channel = 'tasks';
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', api);
+
+mongoose.connect('mongodb://localhost/tasksdb?replicaSet=rs');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection Error:'));
+
+db.once('open', () => {
+  app.listen(9000, () => {
+    console.log('Node server running on port 9000');
+  });
+
+  const taskCollection = db.collection('tasks');
+  const changeStream = taskCollection.watch();
+    
+  changeStream.on('change', (change) => {
+    console.log(change);
+      
+    if(change.operationType === 'insert') {
+      const task = change.fullDocument;
+      pusher.trigger(
+        channel,
+        'inserted', 
+        {
+          id: task._id,
+          task: task.task,
+        }
+      ); 
+    } else if(change.operationType === 'delete') {
+      pusher.trigger(
+        channel,
+        'deleted', 
+        change.documentKey._id
+      );
+    }
+  });
+});
+
