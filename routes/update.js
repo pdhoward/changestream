ddafab4879155e8dc9bfc538cc4766a5eb8674ab
pdhoward <patrick.howard@hotmail.com>
2018-09-20 12:36:05 -0400

    integrate to cms

---
+++ routes/update.js
@@ -0,0 +1,26 @@
+'use strict';
+require('dotenv').config();
+////////////////////////////////////////////////////////
+////////        respond to cms webhook          ///////
+///////////////////////////////////////////////////////
+const bodyParser =  		   require('body-parser')
+const contentful =             require('contentful');
+const Table =                  require('cli-table2')
+const path =                   require('path')
+const sortby =                 require('sort-by')
+const { g, b, gr, r, y } =     require('../console');
+
+
+const update = (router) => {
+
+  router.use(bodyParser.json());
+
+  router.use(function(req, res, next) {
+    console.log(g("Webhook Tests with CMS"))
+    console.log(req.body).pretty()
+    
+  })
+}
+
+
+module.exports = update
