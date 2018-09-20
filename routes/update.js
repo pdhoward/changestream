69708e99909606c35278beb0de3044d4e990fbab
pdhoward <patrick.howard@hotmail.com>
2018-09-20 14:18:16 -0400

    integrate to cms

--- routes/update.js
+++ routes/update.js
@@ -8,6 +8,7 @@ const contentful =             require('contentful');
 const Table =                  require('cli-table2')
 const path =                   require('path')
 const sortby =                 require('sort-by')
+const moment =                 require('moment')
 const { g, b, gr, r, y } =     require('../console');
 
 
@@ -23,7 +24,19 @@ const update = (router) => {
     console.log(r('-----------------------------'))
     console.log(req.body)
     console.log(r('-----------------------------'))
-    console.log(req)
+    console.log(g("Explore Payload"))
+    console.log(req.body.sys.type)
+    console.log(req.body.sys.id)
+    console.log(JSON.stringify(req.body.sys.space))
+    console.log(JSON.stringify(req.body.sys.environment))
+    console.log(JSON.stringify(req.body.sys.contentType))
+    console.log(req.body.sys.revision)
+    let createdAt = moment(req.body.sys.createdAt)
+    let updatedAt = moment(req.body.sys.updatedAt)
+    console.log(createdAt)
+    console.log(updatedAt)
+    console.log(req.body.fields.name)
+    console.log(req.body.fields.message)
     res.end()
 
     next()
