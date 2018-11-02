8606a976854e4c03341b11bc3c88e3f237ddaf8e
pdhoward <patrick.howard@hotmail.com>
2018-11-02 15:39:11 -0400

    updates

---
+++ functions/add.js
@@ -0,0 +1,22 @@
+
+
+
+const After2Seconds = (x) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x * 3);
+    }, 2000);
+  });
+}
+
+const addAsync = async (x) => {
+  const a = await After2Seconds(x);
+  const b = await After2Seconds(20);
+  const c = await After2Seconds(30);
+  return x + a + b + c;
+}
+
+
+addAsync(10).then((sum) => {
+  console.log(sum);
+});
