cd2fdc1f8fda41bf81c023eb4b7241146c91faf2
pdhoward <patrick.howard@hotmail.com>
2018-11-02 10:17:01 -0400

    updates

--- events/index.js
+++ events/index.js
@@ -10,9 +10,12 @@ const {diff} =                  require("deep-diff")
 var ss =                        require('string-similarity');
 const interval =                require('../functions/interval')
 const {queryLists, listen} =    require('../functions/listen')
+const {cbm} =                   require('../data/cbm/bookstore.js')
 const { g, b, gr, r, y } =      require('../console');
 
 const channel = 'tasks';
+
+console.log(cbm)
 
 const pipeline = [
   {
@@ -53,40 +56,6 @@ var pub = new Redis({
     password: redispassword
 })
 
-///////////////////////////////////////////////////////////////////////
-////////////////// CBM  test for string matching against   ///////////
-//////////////////////////////////////////////////////////////////////
-
-const cbm = [
-  { action: 'banter',
-    url: 'https:\\\www.example.com/banter',
-    description: "banter and chit chat",
-    triggers: ['hi', 'hello', 'howdy', 'haps', 'what you doing'] },
-  {
-    action: 'buy',
-    url: 'https:\\\www.example.com/buy',
-    description: "buy the product",
-    triggers: ['i would like to buy', 'buy it', 'i am ready to buy', 
-                'ready to checkout', 'checkout please'] },
-  {
-    action: 'search',
-    url: 'https:\\\www.example.com/search',
-    description: "search for a product",
-    triggers: ['can you find that item for me', 'do you have that item', 
-               'need that item', 'how do i find that item', 'i need to find a product',
-               'can you help me find a product', 'need a product'] },
-  {
-    action: 'ship',
-    url: 'https:\\\www.example.com/ship',
-    description: "ship a product",
-    triggers: ['please ship', 'ship', 'ship this', 'need to send it'] },
-  {
-    action: 'about',
-    url: 'https:\\\www.example.com/about',
-    description: "CMB capability",
-    triggers: ['what do you', 'are you a bot', 'what can you handle'] }
-
-]
 
 
 
