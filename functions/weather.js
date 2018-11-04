3c007d0c42864e822ae70bc07c56118ad475be76
pdhoward <patrick.howard@hotmail.com>
2018-11-04 15:14:59 -0500

    update

--- functions/weather.js
+++ functions/weather.js
@@ -9,13 +9,16 @@ const { EventEmitter } = require('eventemitter3')
 const fetch =            require('node-fetch')
 
 const dark = new EventEmitter();
+let secret = process.env.DARKSKY_SECRET
+const api = "https://api.darksky.net/forecast/" + secret + "/43.7695,11.2558"
 
 const getDarkWeather = async () => {
   try {
-    const obj = await fetch(
-      "https://api.darksky.net/forecast/process.env.DARKSKY_SECRET/43.7695,11.2558"
-    )
-    dark.emit("darkWeather", obj.data.currently.temperature)
+    const res = await fetch( api )
+    const json = await res.json()
+    console.log("WHATS HAPPENING --------------")
+    console.log(json)
+    dark.emit("darkWeather", json.currently.temperature)
   } catch (error) {
     console.error(`Error: ${error.code}`);
   }
