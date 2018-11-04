bf2a1909dc56532aad0f5026cab01ec8790dfaf5
pdhoward <patrick.howard@hotmail.com>
2018-11-04 14:27:45 -0500

    update

--- functions/time.js
+++ functions/time.js
@@ -29,19 +29,3 @@ process.once('uncaughtException', (err) => {
     // FORCE exit the process too.
     process.exit(1);
 });
-/*
-/////////////////////////////////
-///// 3rd example //////////////
-///////////////////////////////
-
-const getApiAndEmit = async socket => {
-  try {
-    const res = await axios.get(
-      "https://api.darksky.net/forecast/PUT_YOUR_API_KEY_HERE/43.7695,11.2558"
-    );
-    socket.emit("FromAPI", res.data.currently.temperature);
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
-};
-*/
