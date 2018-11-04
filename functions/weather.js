2f6effcd0a0a9308e85385451942cc5dec5dd2bf
pdhoward <patrick.howard@hotmail.com>
2018-11-04 14:37:20 -0500

    update

--- functions/weather.js
+++ functions/weather.js
@@ -14,8 +14,8 @@ const getDarkWeather = async () => {
   try {
     const res = await fetch(
       "https://api.darksky.net/forecast/process.env.DARKSKY_SECRET/43.7695,11.2558"
-    );
-    dark.emit("FromAPI", res.data.currently.temperature);
+    )
+    dark.emit("FromAPI", res.data.currently.temperature)
   } catch (error) {
     console.error(`Error: ${error.code}`);
   }
